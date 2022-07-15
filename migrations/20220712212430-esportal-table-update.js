d354df66aae08f4d833c9762c87dd4ccc3d03ec5
Jimmy Jardland <jimmy@jardland.se>
2022-07-15 12:37:15 +0200

    chore: make it possible to revert migration

--- migrations/20220712212430-esportal-table-update.js
+++ migrations/20220712212430-esportal-table-update.js
@@ -3,3 +3,9 @@ exports.up = function (db) {
     change created created DATETIME DEFAULT NOW()
   `)
 }
+
+exports.down = function (db) {
+  return db.runSql(`ALTER TABLE esportal
+  change created created TIME DEFAULT CURRENT_TIME
+  `)
+}
